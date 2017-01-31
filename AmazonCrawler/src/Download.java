9e3e44091971f07d51985adb4c39e1f6e565fb50
(╬☉д⊙) <liuhpleon@gmail.com>
2017-01-30 16:08:00 -0800

    week2 update

---
+++ AmazonCrawler/src/Download.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.net.*;
+
+import javax.swing.event.DocumentEvent;
+// download the page
+public class Download {
+    private String url;
+    private String filename;
+    public Download(String url,String filename){
+    	this.url = url;    }
+    public void core(){
+    	if(filename.length()>0){
+    	try{
+        	URL url = new URL(this.url);
+        	System.out.println(url);
+            try{
+                OutputStream os1 = new FileOutputStream("/E:/CS242Link"+"\\"+this.filename+".txt");
+                BufferedWriter writer1 = new BufferedWriter(new OutputStreamWriter(os1));
+                writer1.write(url.toString());
+                System.out.println(url.toString());
+                writer1.close();
+                os1.close();
+            }catch (Exception e) {
+				System.out.println(e);
+			}
+            URLConnection con = url.openConnection();
+            con.setConnectTimeout(5*1000);
+            InputStream is = con.getInputStream();
+            OutputStream os = new FileOutputStream("/E:/CS242DATA"+"\\"+this.filename+".html");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os));
+            String input = "";
+            while ((input = reader.readLine())!=null) { 
+                writer.write(input+"\n");
+            }
+            writer.close();
+            reader.close();
+            os.close();
+            is.close();
+
+        }catch(Exception e){
+    		System.out.println("this page is not readable");
+    	}
+    	}
+    }
+    // test
+    public static void main(String[] args) {
+    	String url = "https://leetcode.com/problemset/algorithms/";
+    	Download download = new Download(url, "hello");
+    	download.core();
+    }
+}
--- AmazonCrawler/src/Seed.java
+++ AmazonCrawler/src/Seed.java
@@ -4,31 +4,31 @@ import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.*;
 import org.jsoup.parser.*;
-
+import org.json.*;
 public class Seed {
-	// this is where we started
-    private  String url = "https://www.amazon.com/s/ref=nb_sb_noss_2?url=node%3D"
-    		+ "565108&field-keywords=computer&rh=n%3A172282%2Cn%3A5"
-    		+ "41966%2Cn%3A13896617011%2Cn%3A565108%2Ck%3Acomputer";
-    private boolean end = false;
-    public LinkedList<Info> getPage(){
+ 
+    /*this part is to get all the items informations in each pages*/
+    public LinkedList<Info> getPage(String url){
     	LinkedList<Info>seeds = new LinkedList<>();
     	try{
-    		//connect to the website, we need to add userAgent or there is http 503 error
+    		/*connect to the website, we need to add userAgent or there is http 503 error*/
     		Document doc = Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(100000).get();
     		int index = 0;
-    		while(index<25){
-    			String query = "#result_"+index;
-    			Element element = null;
-    			try{
-    			     element = doc.select(query).get(0);
-    			}catch(Exception exception){
-    				System.out.println("end");
-    				end = true;
-    			}
-    			if(end==true) break;
+			String query0 = "#s-results-list-atf";
+			Element es = null;
+			try{
+			     es = doc.select(query0).get(0);
+			}catch(Exception e){
+				System.out.println("read query0 error");
+				System.out.println(e);
+			}
+			Element first = es.child(0);
+			String s = first.attr("id");
+			int start = Integer.parseInt(s.substring(s.length()-2));
+    		for(int i = start;i<start+25;i++){
     			// get all the information from each element
     			// e1 contains name & link (title)
+    			Element element = doc.select("#result_"+i).get(0);
     			Element e1 = element.select("div > div > div > div.a-fixed-left-grid-col.a-col-right >"
     					+ " div.a-row.a-spacing-small > div:nth-child(1) > a").get(0);
     			String name = e1.attr("title");
@@ -37,24 +37,53 @@ public class Seed {
     			//e2 contains img src
     			Element e2 = element.select("div > div > div > div.a-fixed-left-grid-col.a-col-left > div > div > a > img").get(0);
     			String piclink = e2.attr("src");
-    			System.out.println(index+1+" "+name+" "+asin);
-    			System.out.println(link);
-    			System.out.println(piclink);
+    			System.out.println(name+" "+asin);
     			Info info = new Info(name, link, asin, piclink);
     			seeds.add(info);
     		    Thread.sleep(200);
     		    index++;
     		}
     	}catch(Exception e){
-    		System.out.println("error");
+    		System.out.println("read query1 error");
     		System.out.println(e);
     	}
     	return seeds;
     }
-    
+    /*this part is to get next pages by reading the link in icon next*/
+    public String getNext(String url){
+    	String next = "";
+    	try{
+    	    Document doc = Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(100000).get();
+    	    next = doc.select("#pagnNextLink").get(0).attr("href");
+    	}catch (Exception e) {
+    		System.out.println("read nextlink error");
+    		System.out.println(e);
+		}
+    	url = "https://www.amazon.com"+next;
+    	System.out.println(url);
+    	return url;
+    }
+    /*this part is to doing a loop to get all the informations start with starturl*/
+    public LinkedList<Info> loop(String url){
+    	LinkedList<Info>res = new LinkedList<>();
+        for(int i=0;i<30;i++){
+        	res.addAll(getPage(url));
+        	url = getNext(url);
+        	System.out.println("--------------------------------------");
+        	try{
+        	    Thread.sleep(1000);
+        	}catch (Exception e) {
+				// TODO: handle exception
+			}
+        }
+        return res;
+    }
     public static void main(String args[]){
+    	String start = "https://www.amazon.com/s/ref=nb_sb_noss_2?url=node%3D"
+        		+ "565108&field-keywords=computer&rh=n%3A172282%2Cn%3A5"
+        		+ "41966%2Cn%3A13896617011%2Cn%3A565108%2Ck%3Acomputer";
     	Seed seed = new Seed();
-    	seed.getPage();
+    	seed.loop(start);
     }
 }
 //build a class info to save the basic information that we downloaded online
